2c1d3d977cd48358ce887e8f64cfa3cf75e000d5
Bernardo Bruning <bernardo.bruning@gmail.com>
2024-06-05 00:19:23 -0300

    feat: include HTTP proxy support

--- main.go
+++ main.go
@@ -7,14 +7,15 @@ import (
 )
 
 var (
-	port        = flag.String("port", ":11437", "Port to listen on")
-	portSSL     = flag.String("port-ssl", ":11436", "Port to listen on")
-	proxyPort   = flag.String("proxy-port", ":11435", "Proxy port to listen on")
-	cert        = flag.String("cert", "", "Certificate file path *.crt")
-	key         = flag.String("key", "", "Key file path *.key")
-	model       = flag.String("model", "codellama:code", "LLM model to use")
-	numPredict  = flag.Int("num-predict", 50, "Number of predictions to return")
-	templateStr = flag.String("template", "<PRE> {{.Prefix}} <SUF> {{.Suffix}} <MID>", "Fill-in-middle template to apply in prompt")
+	port         = flag.String("port", ":11437", "Port to listen on")
+	proxyPort    = flag.String("proxy-port", ":11438", "Proxy port to listen on")
+	portSSL      = flag.String("port-ssl", ":11436", "Port to listen on")
+	proxyPortSSL = flag.String("proxy-port-ssl", ":11435", "Proxy port to listen on")
+	cert         = flag.String("cert", "", "Certificate file path *.crt")
+	key          = flag.String("key", "", "Key file path *.key")
+	model        = flag.String("model", "codellama:code", "LLM model to use")
+	numPredict   = flag.Int("num-predict", 50, "Number of predictions to return")
+	templateStr  = flag.String("template", "<PRE> {{.Prefix}} <SUF> {{.Suffix}} <MID>", "Fill-in-middle template to apply in prompt")
 )
 
 // main is the entrypoint for the program.
@@ -30,7 +31,8 @@ func main() {
 		NumPredict:  *numPredict,
 	}
 
-	go internal.Proxy(*proxyPort, *portSSL)
+	go internal.Proxy(*proxyPortSSL, *portSSL)
+	go internal.Proxy(*proxyPort, *port)
 
 	go server.Serve()
 	server.ServeTLS()
