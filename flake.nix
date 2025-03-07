a09ef9ffa5476b4c9a5156b1fbd59a677f9aaa85
Tyler Hardin <th020394@gmail.com>
2025-03-06 20:23:16 -0500

    Add flake.nix.

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    {
+      nixosModules.default =
+        { config, lib, ... }:
+        let
+          cfg = config.ollama-copilot;
+        in
+        {
+          options = {
+            ollama-copilot.enable = lib.mkEnableOption "Enable ollama-copilot";
+            ollama-copilot.model = lib.mkOption {
+              type = lib.types.str;
+              default = "";
+              description = "The model to use (default: don't set the option, let the app choose its default)";
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            systemd.services.ollama-copilot =
+              let
+                model_opt = if cfg.model != "" then " --model " + cfg.model else "";
+              in
+              {
+                description = "Ollama Copilot";
+                wantedBy = [ "multi-user.target" ];
+                serviceConfig = {
+                  Type = "simple";
+                  ExecStart = "${lib.getExe self.packages.x86_64-linux.default} ${model_opt}";
+                  Restart = "always";
+                };
+              };
+          };
+        };
+
+      packages.x86_64-linux.default =
+        let
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+        in
+        pkgs.buildGoModule rec {
+          name = "ollama-copilot";
+          src = ./.;
+          vendorHash = "sha256-g27MqS3qk67sve/jexd07zZVLR+aZOslXrXKjk9BWtk=";
+          meta.mainProgram = "ollama-copilot";
+        };
+    };
+}
